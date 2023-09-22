583402e4cc71b444d952a999f7a57df1764d323f
Rafael Adelino Candido <112955398+CandidoRafael@users.noreply.github.com>
2023-09-22 09:43:48 -0300

    refactor: add route soma

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require('express')
 
 const app = express()
 
-app.get('/home', (req, res) => {
-    res.send('Hello World')
-})
+// ROTAS
+// Method HTTP
+// Get - PEga info
+// POST - Cria uma info
+// PUT - Altera toda a info
+// PATCH - Altera parte da info
+// DELETE - Apaga uma info
 
-app.listen(3000)
+// Name - Um identificador da rota
+
+// Function - (callback)
+
+app.get('/home', (req, res) => {
+  const soma = 1 + 230
+
+  res.send({ soma })
+});
+
+app.listen(3000);
